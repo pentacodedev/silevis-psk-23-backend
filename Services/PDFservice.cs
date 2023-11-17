32032c6e2539d74cadfd243dc29d3c84c66821ae
Szymat23 <mateusz.szamkiewicz5@gmail.com>
2023-11-17 18:34:38 +0100

    added solution file

---
+++ Services/PDFservice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+public class PDFservice
+{
+
+	public PDFservice() { };
+
+	public void generate_form_1(Student student, Company company)
+	{
+		string inputFilePath = "Services\\PDFservice.cs";
+	}
+}
