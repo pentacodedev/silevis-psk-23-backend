8ca9b78166e799b9e452e8150c7a80a0e9400065
Szymat23 <mateusz.szamkiewicz5@gmail.com>
2023-11-17 18:37:20 +0100

    fix

--- Services/PDFservice.cs
+++ Services/PDFservice.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.IO;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
-
-public class PDFservice
+﻿public class PDFservice
 {
 
-	public PDFservice() { };
+	public PDFservice() { }
 
-	public void generate_form_1(Student student, Company company)
-	{
-		string inputFilePath = "Services\\PDFservice.cs";
-	}
+	
 }
