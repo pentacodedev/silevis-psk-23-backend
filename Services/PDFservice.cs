4044400a2c79c2e514a6b816f2fd624d300943e0
Szymat23 <mateusz.szamkiewicz5@gmail.com>
2023-11-18 04:24:43 +0100

    PDF_2_form

--- Services/PDFservice.cs
+++ Services/PDFservice.cs
@@ -4,21 +4,15 @@ using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 using HackathonApi.Entities;
 
+
+namespace HackathonApi;
 public class PDFservice
 {
-	
+
 	public PDFservice() { }
 
-
-	public byte[] Form_1_Generate(Company company, User student, Internship internship, bool polish)
+	public string Replace(string form, Company company, User student, Internship internship)
 	{
-		string form = " ";
-
-		if (polish)
-			form = File.ReadAllText("Forms_templ\\Form_Pol_1.txt");
-		else
-			form = File.ReadAllText("Forms_templ\\Form_Eng_1.txt");
-
 		form = form.Replace("#1", DateTime.Now.ToString("dd.MM.yyyy"));
 		form = form.Replace("#2", company.Name);
 		form = form.Replace("#3", company.Address);
@@ -32,7 +26,21 @@ public class PDFservice
 		form = form.Replace("#11", internship.DateOfStart.ToString("dd.mm"));
 		form = form.Replace("#12", internship.DateOfEnd.ToString("dd.mm"));
 
-		return Document.Create(container =>
+		return form;
+	}
+
+	public void Form_1_Generate(Company company, User student, Internship internship, bool polish)
+	{
+		string form = " ";
+
+		if (polish)
+			form = File.ReadAllText("");
+		else
+			form = File.ReadAllText("");
+
+		form = Replace(form, company, student, internship);
+
+		Document.Create(container =>
 		{
 			container.Page(page =>
 			{
@@ -84,9 +92,9 @@ public class PDFservice
 
 
 						handler.SetHtml(@"<p><b>§ 5. 1</b>Strony wyznaczają osoby właściwe do kontaktu w bieżących sprawach:</p>
-                                        <p><b>1)</b> z ramienia Zakładu Pracy <h7>" + company.RepresentativeFirstname+ " " +company.RepresentativeSurname+"</h7><br>" +
+                                        <p><b>1)</b> z ramienia Zakładu Pracy <h7>" + company.RepresentativeFirstname + " " + company.RepresentativeSurname + "</h7><br>" +
 										"tel.: <h7>" + company.PhoneNumber + "</h7><br>" +
-										"e-mail: <h7>" + company.Email+ "</h7><br>" +
+										"e-mail: <h7>" + company.Email + "</h7><br>" +
 										"<b>2)</b> z ramienia Uczelni <h7>Patryk Frączek</h7><br>" +
 										"tel.: <h7>123 123 123</h7><br>" +
 										"e-mail: <h7>p.fraczek@tu.kiece.pl</h7></p>" +
@@ -105,9 +113,96 @@ public class PDFservice
 				});
 			});
 		})
-		 .GeneratePdf();
+		 .GeneratePdfAndShow();
 	}
 
+	public void Form_2_Generate(Company company, User student, Internship internship, bool polish)
+	{
+		string form = " ";
 
+		if (polish)
+			form = File.ReadAllText("");
+		else
+			form = File.ReadAllText("");
+
+		form = Replace(form, company, student, internship);
+
+		Document.Create(container =>
+		{
+			container.Page(page =>
+			{
+				page.Size(PageSizes.A4);
+				page.MarginLeft(2.5f, Unit.Centimetre);
+				page.MarginRight(2.5f, Unit.Centimetre);
+				page.MarginBottom(1.26f, Unit.Centimetre);
+				page.MarginTop(2.56f, Unit.Centimetre);
+				page.PageColor(Colors.White);
+
+
+				page.DefaultTextStyle(TextStyle.Default.FontFamily("Times New Roman"));
+
+				page.Content().Column(col =>
+				{
+					col.Item().HTML(handler =>
+					{
+						handler.SetContainerStyleForHtmlElement("div", c => c.ContentFromRightToLeft());
+						handler.SetContainerStyleForHtmlElement("container", c => c.AlignCenter());
+						handler.SetContainerStyleForHtmlElement("aside", c => c.AlignLeft());
+
+
+						handler.SetHtml(form);
+
+					});
+				});
+
+			});
+
+			container.Page(page =>
+			{
+				page.Size(PageSizes.A4);
+				page.MarginLeft(2.5f, Unit.Centimetre);
+				page.MarginRight(2.5f, Unit.Centimetre);
+				page.MarginBottom(1.26f, Unit.Centimetre);
+				page.MarginTop(2.56f, Unit.Centimetre);
+				page.PageColor(Colors.White);
+
+
+				page.DefaultTextStyle(TextStyle.Default.FontFamily("Times New Roman"));
+
+				page.Content().Column(col =>
+				{
+					col.Item().HTML(handler =>
+					{
+						handler.SetContainerStyleForHtmlElement("div", c => c.ContentFromRightToLeft());
+						handler.SetContainerStyleForHtmlElement("container", c => c.AlignCenter());
+
+
+						handler.SetHtml(@"<div><p><b>Załącznik A</b><br></p></div>
+                                       
+                                       <container><h5>Zaświadczenie</h5></container>
+                                      <p><b>1. Cel wystawienia: </b>zaliczenie praktyki studenckiej<br>
+                                         <b>2. Imię i nazwisko studenta:</b>  " + student.FirstName + " " + student.LastName + @"<br>
+                                      <b>3. Nazwa instytucji/zakładu, w której pracuje/pracował student:</b><br>
+                                      <b>4. Adres instytucji/zakładu</b><br>
+                                     <b>4. Adres instytucji/zakładu</b><br>
+                                     <b>7. Czas pracy</b><br>
+                                     <b>8. Zakres obowiązków studenta w czasie pracy w odniesieniu do kierunku jego studiów</b><br>
+                                     <b>9. Dane osoby, która może poświadczyć prawdziwość powyższych danych (prezes, dyrektor,Koordynator Programu Badawczego/Erasmusa, itp.) wraz z podpisem</b><br>
+                                     <b>10. Uwagi…</b><br>
+                                     <b>11. Data i podpis studenta</b><br>
+                                         </p>");
+
+					});
+				});
+
+			});
+
+
+		})
+		 .GeneratePdfAndShow();
+	}
 }
+
+
+
 
