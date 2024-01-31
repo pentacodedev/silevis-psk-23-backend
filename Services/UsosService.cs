6bf5e6dba50705e5331f2ece6063a7b1f81de5bb
Jacek Jarosz <jacekjarosz55@gmail.com>
2024-01-31 14:56:42 +0100

    mock api offline

--- Services/UsosService.cs
+++ Services/UsosService.cs
@@ -14,8 +14,19 @@ namespace HackathonApi.Services
 
         public async Task<User?> GetUserByEmailAsync(string email)
         {
+            /*
             var result = await _client.GetAsync($"http://hackathon23-mockapi-env.eba-qfrnjqkt.eu-central-1.elasticbeanstalk.com/user/{email}");
             return await result.Content.ReadFromJsonAsync<User>();
+            */
+
+            /* override */
+            return new User {
+                FirstName = "Test",
+                LastName = "Debug",
+                StudentNumber = "1111",
+                StudentProgrammes = new List<StudentProgramme>(),
+                Email = email,
+            };
         }
         
     }
